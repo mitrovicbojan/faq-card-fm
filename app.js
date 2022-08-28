9d97a2b1473d324647294322941b576eb304aa24
unknown <mitrovicbojan87@gmail.com>
2022-08-28 22:47:48 +0300

    js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const questions = document.querySelectorAll(".question");
 questions.forEach(function (e) {
   const btn = e.querySelector(".question-btn");
   btn.addEventListener("click", function () {
+    btn.classList.toggle("rotate");
     questions.forEach(function (i) {
       if (i !== e) {
         i.classList.remove("show-text");
