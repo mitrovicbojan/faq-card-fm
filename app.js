8ce01e314efd3eee67b70e5dfb023cebc097a53d
unknown <mitrovicbojan87@gmail.com>
2022-08-29 20:39:38 +0300

    faq

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const questions = document.querySelectorAll(".question");
 questions.forEach(function (e) {
   const btn = e.querySelector(".question-btn");
   btn.addEventListener("click", function () {
-    btn.classList.toggle("rotate");
     questions.forEach(function (i) {
       if (i !== e) {
         i.classList.remove("show-text");
       }
+      btn.classList.toggle("rotate");
     });
     e.classList.toggle("show-text");
   });
